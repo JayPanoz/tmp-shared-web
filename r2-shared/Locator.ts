7d4f1cdcd2baa0409a04dd6b99a848bc9caab835
JayPanoz <panozpublishingteam@gmail.com>
2020-05-14 15:32:58 +0200

    Add Location props for HTML docs
    cf.
    https://github.com/readium/architecture/blob/master/models/locators/exte
    nsions/html.md

--- r2-shared/Locator.ts
+++ r2-shared/Locator.ts
@@ -4,11 +4,25 @@ interface IText {
   highlight?: string;
 }
 
+interface IStartEnd {
+  cssSelector: string;
+  textNodeIndex: number;
+  charOffset?: number;
+}
+
+interface IDomRange {
+  start: IStartEnd;
+  end?: IStartEnd;
+}
+
 interface ILocations {
   fragments?: Array<string>;
   progression?: number;
   position?: number;
   totalProgression?: number;
+  cssSelector?: string;
+  partialCfi?: string;
+  domRange?: IDomRange;
 }
 
 export interface ILocator {
